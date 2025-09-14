0dc9598a3df69f7e112a39a6bb0683bfa2e793cb
Jaredm20 <aurier256@gmail.com>
2025-09-14 13:41:20 -0500

    Sub

--- waitlist_manager.py
+++ waitlist_manager.py
@@ -6,7 +6,9 @@ class Node:
         name (str): The name of the customer.
         next (Node): A reference to the next node in the list.
     '''
-    
+    def __init__(self, name):
+        self.name = name
+        self.next = None
     
 
 
@@ -23,12 +25,54 @@ class LinkedList:
         remove(name): Removes a customer from the waitlist by name.
         print_list(): Prints the current waitlist.
     '''
+    def __init__(self):
+        self.head = None
+
+    def add_front(self, name):
+        new_node = Node(name)
+        new_node.next = self.head
+        self.head = new_node
+
+    def add_end(self, name):
+        new_node = Node(name)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def remove(self, name):
+        current = self.head
+        previous = None
+        while current:
+            if current.name == name:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                return True
+            previous = current
+            current = current.next
+        return False
+
+    def print_list(self):
+        current = self.head
+        if not current:
+            print("Waitlist is empty.")
+            return
+        while current:
+            print(current.name, end=" -> " if current.next else "")
+            current = current.next
+        print()
     
 
 
 def waitlist_generator():
     # Create a new linked list instance
     
+    waitlist = LinkedList()
     
     while True:
         print("\n--- Waitlist Manager ---")
@@ -42,25 +86,26 @@ def waitlist_generator():
         
         if choice == "1":
             name = input("Enter customer name to add to front: ")
+            waitlist.add_front(name)
+            print(f"{name} added to the front")
             # Call the add_front method
             
 
         elif choice == "2":
             name = input("Enter customer name to add to end: ")
-            # Call the add_end method
-            
+            waitlist.add_end(name)
+            print(f"{name} added to the end.")
 
         elif choice == "3":
             name = input("Enter customer name to remove: ")
-            # Call the remove method
-            
+            if waitlist.remove(name):
+                print(f"{name} removed from waitlist.")
+            else:
+                print(f"{name} not found in waitlist.")
             
         elif choice == "4":
             print("Current waitlist:")
-            # Print out the entire linked list using the print_list method.
-            
-            
-            
+            waitlist.print_list()
 
         elif choice == "5":
             print("Exiting waitlist manager.")
@@ -69,11 +114,15 @@ def waitlist_generator():
             print("Invalid option. Please choose 1–5.")
 
 # Call the waitlist_generator function to start the program
-
+waitlist_generator()
 
 '''
 Design Memo: Write Your Design Memo Include a 200–300 word response in your code or in a .txt file:
 - How does your list work?
 - What role does the head play?
 - When might a real engineer need a custom list like this?
+
+My list works by storing each inserted customer as a node. The program works by referencing the position of the head (first item in list essentially) and then going down the list from there. When it comes to adding or subtracting a name from the list, the sequence references the head node and moves from there.
+The head is what allows for all the functions in the menu to work, the functions act as pointers around the head.
+Theres multiple ways you could use a list designed like this for, one of them could be a to-do list because engineering jobs typically stack up. Maybe the engineer is making a wait list for a restaurant that needs to have some sort of list that can adapt rather than set in stone. 
 '''
